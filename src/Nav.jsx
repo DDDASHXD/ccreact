b3016cd4aafe7b9600a87a0c97743b5063ba9a78
july-bht <83638794+july-bht@users.noreply.github.com>
2022-04-03 17:13:30 +0200

    json

---
+++ src/Nav.jsx
@@ -0,0 +1,24 @@
+import { Navbar, Container } from "../node_modules/react-bootstrap";
+
+const Nav = () => {
+  return (
+    <Navbar className="nav" expand="lg">
+      <Container>
+        <Navbar.Brand to="/home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link to="/home">Home</Nav.Link>
+            <Nav.Link to="/omkring">Omkring</Nav.Link>
+            <Nav.Link to="/behandler">Behandlinger</Nav.Link>
+            <Nav.Link to="/kontakt">Kontakt</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <button className="nav__btn"></button>
+      </Container>
+    </Navbar>
+    
+  );
+};
+
+export default Nav;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
 import "../style/hero.scss";
+import data from "../assets/text.json";
 
 const Hero = () => {
-    return (
-        <div className="hero">
-            <div className="hero__container">
-                <h1 className="hero__title">
-                    Liv og karriere <br /> coaching
-                </h1>
-                <p className="home__description"></p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="hero">
+      <div className="hero__container">
+        <h1 className="hero__title">{data.home[0].title}</h1>
+        <span className="hero__subtitle">{data.home[0].subtitle}</span>
+        <p className="home__description">{data.home[0].description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
