699e01e43390bf57f89d070e1c7440e9eaef8815
Yes <dddashxd@gmail.com>
2022-05-01 20:38:05 +0200

    Cards setup

--- src/components/Bestilling/treatment.jsx
+++ src/components/Bestilling/treatment.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useeffect } from "react";
-import { Title } from "@mantine/core";
+import { Title, ScrollArea } from "@mantine/core";
 import "../../style/bestilling/treatment.scss";
 import data from "../../assets/text.json";
 
 const Treatment = (props) => {
   return (
-    <section className="treatment section">
-      <Title order={3}>Behandling</Title>
-      <div className="content">
+    <ScrollArea height={"100%"} className="treatment section">
+      <div className="treatment-grid">
         {data.Behandlinger.map((item, index) => (
-          <p>{item.name}</p>
+          <div className="treatment-card">
+            <div className="treatment-card__header">
+              <Title order={3}>{item.name}</Title>
+            </div>
+          </div>
         ))}
       </div>
-    </section>
+    </ScrollArea>
   );
 };
 
