eb4ccebfa775134d66e11f21f66b8d650e33e51b
july-bht <83638794+july-bht@users.noreply.github.com>
2022-05-03 17:40:44 +0200

    text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,30 @@ const Footer = () => {
     <footer className="footer container-fluid p-0">
       <Row className="p-5 ">
         <Col md={4} className="">
-          <Row className=" d-flex justify-content-center text-center">
+          <Row className="  text-center">
+            <span>Just Div</span>
             <h2 className="title">Website Creators</h2>
-            <div className="footer__icons d-flex justify-content-center gap-1">
+            <Col className="px-5">
+              {" "}
+              <p>
+                Hjemmesiden er lavet af et lille selvstartet web firma ved navn JustDiv
+                betånde af 2 personer. Julie er front end designer og developer og Sebastian er backend developer og har
+                <br class="d-none d-md-block" />
+                JustDiv laver hjemmesider og grafisk design til alle slags
+                virksomheder.
+              </p>
+              <p>Sebastian Skov NIelsen & Julie Boeriis Thomhav</p>
+            </Col>{" "}
+            <Col
+              xs={12}
+              className=" footer__icons d-flex justify-content-center gap-1"
+            >
               <i className="bx bx-linkedin"></i>
               <i className="bx bx-facebook"></i>
               <i className="bx bx-linkedin"></i>
               <i className="bx bx-linkedin"></i>
               <i className="bx bx-linkedin"></i>
-            </div>
+            </Col>
           </Row>
         </Col>
 
